32a427b1b52162c7dad466b5ee7d2a5250cfd2f9
jacosta2002 <137431220+jacosta2002@users.noreply.github.com>
2023-07-21 17:12:47 -0700

    Update and rename test..cpp to Example.java

---
+++ src/Example.java
@@ -0,0 +1,6 @@
+public class Example {
+    public static void main(String[] args) {
+        String password = "password123";
+        System.out.println("Your password is: " + password);
+    }
+}
