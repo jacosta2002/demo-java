f6afdd7c41ebeacb1df6dbe338103a62cbca743b
jacosta2002 <137431220+jacosta2002@users.noreply.github.com>
2023-07-24 09:53:16 -0700

    Create XSSController.java

---
+++ src/main/java/com/github/hackathon/advancedsecurityjava/Controllers/XSSController.java
@@ -0,0 +1,30 @@
+package com.github.hackathon.advancedsecurityjava.Controllers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.hackathon.advancedsecurityjava.Application;
+import com.github.hackathon.advancedsecurityjava.Models.Book;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SQLInjectionController {
+
+  private static Connection connection;
+
+  @GetMapping("/greeting")
+	public String index(@RequestParam(name="name", required=true) String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+  }
+}
