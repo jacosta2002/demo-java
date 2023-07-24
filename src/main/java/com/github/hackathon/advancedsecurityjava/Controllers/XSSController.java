b778acf707961145b97fa6f24faf5c3ae719f28c
jacosta2002 <137431220+jacosta2002@users.noreply.github.com>
2023-07-24 09:55:12 -0700

    Update XSSController.java

--- src/main/java/com/github/hackathon/advancedsecurityjava/Controllers/XSSController.java
+++ src/main/java/com/github/hackathon/advancedsecurityjava/Controllers/XSSController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class SQLInjectionController {
+public class XSSController {
 
   private static Connection connection;
 
