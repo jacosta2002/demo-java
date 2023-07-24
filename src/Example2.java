c34cf73f54c01b29929d364d414a6861f85b019c
jacosta2002 <137431220+jacosta2002@users.noreply.github.com>
2023-07-24 09:45:49 -0700

    Delete Example2.java

--- src/Example2.java
+++
@@ -1,8 +0,0 @@
-public class Example2 {
-    public static void main(String[] args) {
-        String username = "admin'; DROP TABLE users; --";
-        String password = "password123";
-        String query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
-        System.out.println("Your query is: " + query);
-    }
-}
