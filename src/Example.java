18cb8d13acdfa063577cf6bdd624fd4b6eda7138
jacosta2002 <137431220+jacosta2002@users.noreply.github.com>
2023-07-24 09:45:44 -0700

    Delete Example.java

--- src/Example.java
+++
@@ -1,6 +0,0 @@
-public class Example {
-    public static void main(String[] args) {
-        String password = "password123";
-        System.out.println("Your password is: " + password);
-    }
-}
